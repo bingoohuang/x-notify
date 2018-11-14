9f880fa10a38f1def5254c7e61019c5e53b3ee1f
bingoo <bingoo.huang@gmail.com>
2018-11-14 16:40:54 +0800

    OkHtttpClient shares the same connection pool

--- src/main/java/com/github/bingoohuang/xnotify/util/OkHttp.java
+++ src/main/java/com/github/bingoohuang/xnotify/util/OkHttp.java
@@ -8,13 +8,13 @@ import java.net.Proxy;
 import java.util.Map;
 
 public class OkHttp {
-    static final MediaType JSON_MEDIA_TYPE = MediaType.parse("application/json; charset=utf-8");
+    public static final OkHttpClient CLIENT = new OkHttpClient();
 
     @SneakyThrows
     public static String postJSON(String url, String json, Proxy proxy) {
-        val body = RequestBody.create(JSON_MEDIA_TYPE, json);
+        val body = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), json);
         val request = new Request.Builder().url(url).post(body).build();
-        return execute(request, proxy);
+        return getClient(proxy).newCall(request).execute().body().string();
     }
 
     @SneakyThrows
@@ -25,30 +25,35 @@ public class OkHttp {
         }
 
         val request = new Request.Builder().url(url).post(bodyBuilder.build()).build();
-
-        return execute(request, proxy);
+        return getClient(proxy).newCall(request).execute().body().string();
     }
 
 
     @SneakyThrows
     public static String encodedGet(String url, Map<String, String> encodedQueryParameters, Proxy proxy) {
-        val urlBuilder = HttpUrl.parse(url).newBuilder();
-        for (val e : encodedQueryParameters.entrySet()) {
-            urlBuilder.addEncodedQueryParameter(e.getKey(), e.getValue());
-        }
+        val urlBuilder = HttpUrl.get(url).newBuilder();
+        encodedQueryParameters.forEach(urlBuilder::addEncodedQueryParameter);
 
         val request = new Request.Builder().url(urlBuilder.build()).get().build();
-
-        return execute(request, proxy);
+        return getClient(proxy).newCall(request).execute().body().string();
     }
 
+    /**
+     * 获得OkHttpClient实例。
+     * proxy对象示例:
+     * <code>
+     * Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("192.168.1.105", 8081);
+     * </code>
+     *
+     * @param proxy 代理对象。
+     * @return 定制化后的OkHttpClient对象。
+     */
     @SneakyThrows
-    private static String execute(Request request, Proxy proxy) {
-        val clientBuilder = new OkHttpClient.Builder();
-        if (proxy != null) clientBuilder.proxy(proxy);
-
-        val client = clientBuilder.build();
-        val response = client.newCall(request).execute();
-        return response.body().string();
+    public static OkHttpClient getClient(Proxy proxy) {
+        // https://square.github.io/okhttp/3.x/okhttp/okhttp3/OkHttpClient.html
+        // You can customize a shared OkHttpClient instance with newBuilder().
+        // This builds a CLIENT that shares the same connection pool, thread pools, and configuration.
+        // Use the builder methods to configure the derived CLIENT for a specific purpose.
+        return proxy != null ? CLIENT.newBuilder().proxy(proxy).build() : CLIENT;
     }
 }
